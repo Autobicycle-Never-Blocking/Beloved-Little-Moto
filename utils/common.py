f0573d5b9ab98aa41d33c60dbb95fed39577fbf3
zhaoxingrong <zhaoxingrong@outlook.com>
2020-05-15 22:09:46 +0800

    start

---
+++ utils/common.py
@@ -0,0 +1,15 @@
+from faker import Faker
+
+user_agent = Faker('zh-CN').user_agent()
+
+import time
+
+
+def get_header():
+    return {
+        'User-Agent': user_agent
+    }
+
+
+def get_time():
+    return int(time.time())
